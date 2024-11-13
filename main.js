29b6091f72e8934cc3404afcf8a8492ce99e94ea
BhargavM12 <bhargava.modukuru@torontomu.ca>
2024-11-13 00:16:14 -0500

    lab 8 video

--- main.js
+++ main.js
@@ -48,30 +48,3 @@ function toggleColors() {
     }
 }
 
-let slideIndex = 0;
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-
-    // Reset all slides to be hidden
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Increment the slide index
-    slideIndex++;
-
-    // Reset to the first slide if it's the end
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-
-    // Display the current slide
-    slides[slideIndex - 1].style.display = "block";
-
-    // Call the function every 5 seconds
-    setTimeout(showSlides, 5000);
-}
-
-// Initialize the slideshow
-showSlides();
